6154f8557110b32ea333c8c22297d77c51c000c8
moklidia <kitsunegakkou@gmail.com>
2019-08-25 20:10:37 +0400

    add models/validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,19 @@
 # frozen_string_literal: true
 
 class Article < ApplicationRecord
+  belongs_to :author, class_name 'User'
+
+  validates :name, presence: true
+  validates :body, presence: true
+  validates :author, presence:true
+
+  state_machine initial: :draft do
+  event :publish do
+    transition draft: :published
+  end
+
+  event :archive do
+    transition [:draft, :published] => :archived
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_many :my_articles, class_name: 'Article', foreign_key: :author_id
   has_secure_password
+
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, { with: /.*@.*/ }, uniqueness: true
 end
